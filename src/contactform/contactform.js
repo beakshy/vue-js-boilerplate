7b2c7137146f9af7e7bae988f32b3b9f1d638d19
beakshy <beakshy@gmail.com>
2018-07-30 22:54:31 -0700

    adding sub component

---
+++ src/contactform/contactform.js
@@ -0,0 +1,19 @@
+export default {
+  name: 'contactform',
+  components: {},
+  props: [],
+  data () {
+    return {
+
+    }
+  },
+  computed: {
+
+  },
+  mounted () {
+
+  },
+  methods: {
+
+  }
+}
---
+++ src/contactform/index.vue
@@ -0,0 +1,4 @@
+<template src="./contactform.html"></template>
+<script src="./contactform.js"></script>
+<style src="./contactform.scss" scoped lang="scss"></style>
+
